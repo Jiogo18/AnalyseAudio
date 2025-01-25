06d442f2a45922323c3bb93f155c3e484e0071e7
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2025-01-25 16:27:13 -0500

    Update to .NET 8

---
+++ AnalyseAudio/Views/Pages/DataStream.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AnalyseAudio.Views
+{
+    /// <summary>
+    /// A page to send the FTT datas trough web socket
+    /// </summary>
+    public sealed partial class DataStream : Page
+    {
+        public DataStream()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
