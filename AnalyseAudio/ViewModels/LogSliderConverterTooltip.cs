ef90b24ee44870187ee5a2cebc272b76058b367c
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2025-01-26 20:25:22 -0500

    Generalize FrequencySlider as LogSlider, and add CenterValue parameter
    The slider uses logarithmic-like scale.
    
    However, CenterValue allows the user shift the values within the slider.
    Min and Max doesn't move, but CenterValue will be the center of the slider.
    CenterValue could be 440Hz, 1760Hz, volume 0.1/1...
    
    The user can disable this feature to get a logarithmic scale by setting CenterValue to the value of Minimum.
    Note that if Maximum is close to 1, then the scale is not logarithmic with this implementation. So it is recommended to set CenterValue.

---
+++ AnalyseAudio/ViewModels/LogSliderConverterTooltip.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace AnalyseAudio.ViewModels
+{
+    /// <summary>
+    /// Convert a linear value [0;1000] into a Frequency [1;20000]
+    /// 
+    /// Due to limitations, if Maximum is close to 1, then the scale won't be logarithmic.
+    /// As such, <see cref="InternalScale"/> and set CenterValue.
+    /// </summary>
+    internal class LogSliderTooltipConverter : DependencyObject, IValueConverter
+    {
+        public static readonly DependencyProperty MinimumProperty = DependencyProperty.Register("Minimum", typeof(double), typeof(LogSliderTooltipConverter), new PropertyMetadata(0.0));
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register("Maximum", typeof(double), typeof(LogSliderTooltipConverter), new PropertyMetadata(20000.0));
+        public static readonly DependencyProperty CenterValueProperty = DependencyProperty.Register("CenterValue", typeof(double), typeof(LogSliderTooltipConverter), new PropertyMetadata(0.0));
+
+        public double Minimum
+        {
+            get => (double)GetValue(MinimumProperty);
+            set => SetValue(MinimumProperty, value);
+        }
+        public double Maximum
+        {
+            get => (double)GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
+        }
+        public double CenterValue
+        {
+            get => (double)GetValue(CenterValueProperty);
+            set => SetValue(CenterValueProperty, value);
+        }
+
+        // Linear values of the slider (internal values)
+        public static readonly double MinLinear = 0;
+        public static readonly double MaxLinear = 1000;
+
+        /// <summary>
+        /// Change the repartition of the values between Min and Max.
+        /// Min and Max don't change.
+        /// 
+        /// e.g. Set 440 in the center of the slider
+        /// 
+        /// s=\frac{\left(20000-880\right)}{440^{2}}
+        /// \left(x\right)=\frac{\ln\left(x\cdot s+1\right)}{\ln\left(20000\cdot s+1\right)}\cdot1000
+        /// 
+        /// To disable this and keep a scale fully logarithmic, set CenterValue to Minimum.
+        /// </summary>
+        private double InternalScale => CenterValue == Minimum ? 1 : (Maximum - 2 * CenterValue) / (CenterValue * CenterValue);
+
+        public double ValueToLinear(double freq) =>
+            Math.Log((freq - Minimum) * InternalScale + 1)
+            / Math.Log((Maximum - Minimum) * InternalScale + 1)
+            * (MaxLinear - MinLinear) + MinLinear;
+        public double LinearToValue(double value) =>
+            (Math.Pow((Maximum - Minimum) * InternalScale + 1,
+                (value - MinLinear) / (MaxLinear - MinLinear)) - 1) / InternalScale + Minimum;
+
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            double val = LinearToValue((double)value);
+            if (val == 0) return "0";
+            return val >= 100 ? Math.Round(val) : Math.Round(val, 2 - (int)Math.Floor(Math.Log10(val)));
+        }
+
+        public object ConvertBack(object freq, Type targetType, object parameter, string culture) => throw new NotImplementedException();
+    }
+}
---
+++ AnalyseAudio/Views/Spectrogram/LogSlider.xaml.cs
@@ -0,0 +1,74 @@
+﻿using AnalyseAudio.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AnalyseAudio.Views
+{
+    /// <summary>
+    /// A Slider that can be used (originally for the frequency of the spectrogram).
+    /// The slider is logarithmic-like so moving of X pixels will always multiply the frequency with a factor Y.
+    /// The slider is centered at 440 Hz (CenterValue) <see cref="LogSliderTooltipConverter.InternalScale"/>.
+    /// </summary>
+    public sealed partial class LogSlider : Grid, INotifyPropertyChanged
+    {
+        public static readonly DependencyProperty StepFrequencyProperty = DependencyProperty.Register("StepFrequency", typeof(double), typeof(LogSlider), new PropertyMetadata(0.1));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(LogSlider), new PropertyMetadata(0.0));
+        private LogSliderTooltipConverter scaleConverter = new();
+        public double MinLinear => LogSliderTooltipConverter.MinLinear;
+        public double MaxLinear => LogSliderTooltipConverter.MaxLinear;
+        public double Minimum
+        {
+            get => scaleConverter.Minimum;
+            set => scaleConverter.Minimum = value;
+        }
+        public double Maximum
+        {
+            get => scaleConverter.Maximum;
+            set => scaleConverter.Maximum = value;
+        }
+        public double CenterValue
+        {
+            get => scaleConverter.CenterValue;
+            set => scaleConverter.CenterValue = value;
+        }
+
+        public double StepFrequency
+        {
+            get => (double)GetValue(StepFrequencyProperty);
+            set => SetValue(StepFrequencyProperty, value);
+        }
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set
+            {
+                if (Value == value || EcartRelatif(Value, value) < 0.001) return;
+                SetValue(ValueProperty, value);
+                OnPropertyChanged(nameof(ValueLinear));
+            }
+        }
+
+        private double ValueLinear
+        {
+            get => scaleConverter.ValueToLinear(Value);
+            set { Value = scaleConverter.LinearToValue(value); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        public LogSlider()
+        {
+            InitializeComponent();
+        }
+
+        static double EcartRelatif(double a, double b) => Math.Abs((a - b) / (a != 0 ? a : 1));
+    }
+}
